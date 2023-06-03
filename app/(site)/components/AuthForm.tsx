f139f6298c4e42eac26b4362b553db9373fbaf5c
narspi <ivanovmichurina@gmail.com>
2023-06-03 15:27:00 +0300

    hide variables

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -24,7 +24,7 @@ const AuthForm = () => {
     if (session?.status === 'authenticated') {
       router.push('/users')
     }
-  },[session?.status])
+  },[session?.status,router])
 
   const toogleVariant = useCallback(() => {
     if (variant === "LOGIN") {
---
+++ app/users/layout.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from "react";
+import Sidebar from '../components/sidebar/Sidebar';
+
+export default async function UsersLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  return (
+    // @ts-expect-error Server component
+    <Sidebar>
+      <div className="h-full">{children}</div>
+    </Sidebar>
+  );
+}
