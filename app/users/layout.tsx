ddada9327a3e5812f587fd8e7d698b598cf5f528
narspi <ivanovmichurina@gmail.com>
2023-06-04 17:29:35 +0300

    create conversation on database

--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -1,14 +1,19 @@
 import { ReactNode } from "react";
 import Sidebar from '../components/sidebar/Sidebar';
+import getUsers from '../actions/getUsers';
+import UserList from "./components/UserList";
 
 export default async function UsersLayout({
   children,
 }: {
   children: ReactNode;
 }) {
+  const users = await getUsers();
+
   return (
     // @ts-expect-error Server component
     <Sidebar>
+      <UserList items={users} />
       <div className="h-full">{children}</div>
     </Sidebar>
   );
